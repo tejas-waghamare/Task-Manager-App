64dc4d4673126c541dea2269608fb8f2740b08c9
tejas-waghamare <tejaswaghamare121@gmail.com>
2026-06-06 01:30:27 +0530

    emoji remove

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
-    console.log('✅ MongoDB Connected Successfully.....!✨🌐');
-    console.log('📊 MongoDB Database:', mongoose.connection.db.databaseName);
+    console.log('MongoDB Connected Successfully.....!✨🌐');
+    console.log(' MongoDB Database:', mongoose.connection.db.databaseName);
   } catch (err) {
-    console.error('❌MongoDB Connection Error:.....!⭕', err.message);
+    console.error('MongoDB Connection Error:.....!', err.message);
     process.exit(1);
   }
 };
