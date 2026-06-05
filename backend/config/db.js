c90d918c63db841f512be9a41ae3824b08921797
tejas-waghamare <tejaswaghamare121@gmail.com>
2026-06-06 01:37:50 +0530

    remove star

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
-    console.log('MongoDB Connected Successfully.....!✨🌐');
+    console.log('MongoDB Connected Successfully.....!🌐');
     console.log(' MongoDB Database:', mongoose.connection.db.databaseName);
   } catch (err) {
     console.error('MongoDB Connection Error:.....!', err.message);
